07495f7ba5bffbf40f956c372ced2174bd1431cb
dragoslavberberski-hub <dragoslavberberski@gmail.com>
2025-12-28 22:12:53 +0100

    Prvi commit - dodavanje konvertora valuta
    This program converts an amount in euros to selected currencies.

---
+++ main.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int main() {
+    double iznos;
+    int izbor;
+    double rezultat;
+
+    printf("Unesite iznos u evrima: ");
+    scanf("%lf", &iznos);
+
+    printf("Izaberite valutu za konverziju:\n");
+    printf("1. Dinar (RSD)\n");
+    printf("2. Dolari (USD)\n");
+    printf("3. Funti (GBP)\n");
+    printf("Vas izbor: ");
+    scanf("%d", &izbor);
+
+    switch(izbor) {
+        case 1:
+            rezultat = iznos * 117.5;
+            printf("%.2lf EUR = %.2lf RSD\n", iznos, rezultat);
+            break;
+        case 2:
+            rezultat = iznos * 1.1;
+            printf("%.2lf EUR = %.2lf USD\n", iznos, rezultat);
+            break;
+        case 3:
+            rezultat = iznos * 0.88;
+            printf("%.2lf EUR = %.2lf GBP\n", iznos, rezultat);
+            break;
+        default:
+            printf("Nepoznat izbor!\n");
+    }
+
+    return 0;
+}
