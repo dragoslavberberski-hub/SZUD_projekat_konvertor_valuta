0e4a0ea55067f87b78d41b07e0fcaa9d2d3eb31c
dragoslavberberski-hub <dragoslavberberski@gmail.com>
2025-12-29 00:58:10 +0100

    Promena ispisa CHF – test 1

--- main.c
+++ main.c
@@ -32,7 +32,7 @@ int main() {
             break;
         case 4:
     rezultat = iznos * 0.97; // EUR -> CHF
-   printf("%.2lf EUR = %.2lf CHF (Švajcarski franak)\n", iznos, rezultat);
+   printf("%.2lf EUR = %.2lf CHF (test 1)\n", iznos, rezultat);
 
     break;
         default:
